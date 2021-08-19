56b39b35361a13022318b6e719749fabc21fd436
jin <toto006521@gmail.com>
2021-08-19 21:19:20 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/cheoljinJu/Login/Login.js
+++ src/pages/cheoljinJu/Login/Login.js
@@ -5,6 +5,23 @@ import '../../../styles/reset.scss';
 import '../../../styles/common.scss';
 
 class LoginCheoljin extends Component {
+  state = {
+    id: '',
+    password: '',
+  };
+
+  handleInputId = event => {
+    const id = event.target.value;
+    const state = { ...this.state, id };
+    this.setState(state);
+  };
+
+  handleInputPassword = event => {
+    const password = event.target.value;
+    const state = { ...this.state, password };
+    this.setState(state);
+  };
+
   render() {
     return (
       <div className="login">
@@ -16,13 +33,14 @@ class LoginCheoljin extends Component {
               placeholder="전화번호, 사용자 이름 또는 이메일"
               className="login-form__id"
               aria-label="전화번호, 사용자 이름 또는 이메일"
-              ref={this.idRef}
+              onChange={this.handleInputId}
             />
             <input
               type="password"
               placeholder="비밀번호"
               className="login-form__pw"
               aria-label="비밀번호"
+              onChange={this.handleInputPassword}
             />
             <button type="button" className="login-form__submit">
               로그인
