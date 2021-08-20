220bc5b7061b1b457027010edf72842e90234bce
jin <toto006521@gmail.com>
2021-08-20 18:51:22 +0900

    Fix : 메인 페이지 내 네비 컴포넌트 임포트 경로 수정

--- src/pages/cheoljinJu/Main/Main.js
+++ src/pages/cheoljinJu/Main/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Nav from '../../../components/Nav';
+import Nav from '../../../components/Nav/Nav';
 import './Main.scss';
 
 class MainCheoljin extends Component {
