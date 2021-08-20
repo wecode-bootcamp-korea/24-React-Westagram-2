1a96870b9bf666edb0bd8328e58738810d2d97b6
박은정 <eunjeong@bag-eunjeong-ui-MacBookAir.local>
2021-08-20 20:09:53 +0900

    Add : [Mission 1, 2] input 입력값 저장 및 로그인버튼 구현 - handleInout 함수를 이용해서 input에 입력한 값 저장 - 아이디 "@" 포함되고 비밀번호 5글자이상일 때 버튼 활성화 로직구현완료

--- src/pages/eunjungPark/Login/Login.js
+++ src/pages/eunjungPark/Login/Login.js
@@ -8,9 +8,20 @@ class LoginEunjung extends Component {
     this.state = {
       id: '',
       pw: '',
-      bottonChange: 'login-btn',
     };
   }
+
+  handleInput = event => {
+    //IdInout은 event의 약자 e를 인자로 받음
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  mainGo = () => {
+    this.props.history.push('/main-eunjungPark');
+  };
+
   loginButton = () => {
     const { id, pw } = this.state;
     const checkId = id.includes('@');
@@ -18,16 +29,15 @@ class LoginEunjung extends Component {
 
     if (checkId && checkPw) {
       alert('로그인 성공');
-      this.props.history.push('/main-eunjungPark');
-    }
+      this.mainGo();
+    } else {
+      if (!checkId) {
+        alert('아이디에는 @가 포함되어야 합니다.');
+      }
 
-    if (!checkId) {
-      alert('아이디에는 @가 포함되어야 합니다.');
-    }
-
-    if (!checkPw) {
-      alert('비밀번호는 4자리 이상이어야 합니다.');
-      this.isLoginOK = false;
+      if (!checkPw) {
+        alert('비밀번호는 4자리 이상이어야 합니다.');
+      }
     }
   };
 
@@ -43,10 +53,33 @@ class LoginEunjung extends Component {
               id="login-id"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              name="id"
+              onChange={this.handleInput}
+              // onKeyUp={this.loginButton}
             />
-            <input id="login-pwd" type="password" placeholder="비밀번호" />
+            <input
+              id="login-pwd"
+              type="password"
+              placeholder="비밀번호"
+              name="pw"
+              onChange={this.handleInput}
+              // onKeyUp={this.loginButton}
+            />
             <Link to="/main-eunjung">
-              <button className="login-btn" onClick={this.loginButton}>
+              <button
+                onClick={this.loginButton}
+                tabIndex="-1"
+                className={
+                  this.state.id.indexOf('@') !== -1 && this.state.pw.length > 5
+                    ? 'login-btn-yes'
+                    : 'login-btn-no'
+                }
+                disabled={
+                  this.state.id.indexOf('@') !== -1 && this.state.pw.length > 5
+                    ? false
+                    : true
+                }
+              >
                 로그인
               </button>
             </Link>
