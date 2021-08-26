ef5575bc1809344106144647e15e6e76a0517fd9
jin <toto006521@gmail.com>
2021-08-26 17:10:13 +0900

    💄 Add : footer, aside 컴포넌트 추가

--- src/pages/cheoljinJu/Main/Aside/Aside.js
+++ src/pages/cheoljinJu/Main/Aside/Aside.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from '../Footer/Footer';
 import './Aside.scss';
 
 class Aside extends Component {
@@ -85,44 +86,7 @@ class Aside extends Component {
             </div>
           </div>
         </div>
-        <footer className="footer">
-          <ul>
-            <li>
-              <a href="#">소개</a>
-            </li>
-            <li>
-              <a href="#">도움말</a>
-            </li>
-            <li>
-              <a href="#">홍보 센터</a>
-            </li>
-            <li>
-              <a href="#">API</a>
-            </li>
-            <li>
-              <a href="#">채용 정보</a>
-            </li>
-            <li>
-              <a href="#">개인정보처리방침</a>
-            </li>
-            <li>
-              <a href="#">약관</a>
-            </li>
-            <li>
-              <a href="#">위치</a>
-            </li>
-            <li>
-              <a href="#">인기 계정</a>
-            </li>
-            <li>
-              <a href="#">해시태그</a>
-            </li>
-            <li>
-              <a href="#">언어</a>
-            </li>
-          </ul>
-          <span>© 2021 INSTAGRAM FROM FACEBOOK</span>
-        </footer>
+        <Footer />
       </aside>
     );
   }
---
+++ src/pages/cheoljinJu/Main/Footer/FOOTER_LINK.js
@@ -0,0 +1,15 @@
+const FOOTER_LINK = [
+  { id: 1, title: '소개' },
+  { id: 2, title: '도움말' },
+  { id: 3, title: '홍보 센터' },
+  { id: 4, tilte: 'API' },
+  { id: 5, title: '채용 정보' },
+  { id: 6, title: '개인정보처리방침' },
+  { id: 7, title: '약관' },
+  { id: 8, title: '위치' },
+  { id: 9, title: '인기 계정' },
+  { id: 10, title: '해시태그' },
+  { id: 11, title: '언어' },
+];
+
+export default FOOTER_LINK;
